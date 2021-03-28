bb9fa55b109738efb41e6a704e165186469b44c0
RMaeng <mianromancis@gmail.com>
2021-03-28 15:05:52 +0300

    Установлен webpack
    npm install -D webpack webpack-cli

---
+++ WebSPA/webpack.config.js
@@ -0,0 +1,10 @@
+﻿const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './Client/index.jsx',
+    output: {
+        path: path.resolve(__dirname, './wwwroot/dist'),
+        filename: 'bundle.js'
+    }
+};
